efd94b9cd9e0c4233a6c6b7fd61850ea27f84309
Mohammed Firdous <124298708+mohammedfirdouss@users.noreply.github.com>
2026-09-04 18:36:06 +0100

    fix: add missing Link import in Journal component

--- src/pages/Journal.tsx
+++ src/pages/Journal.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import { getEntries, deleteEntry } from "../lib/journal";
 import type { JournalEntry } from "../lib/types";
 
